877f3074d785ec9782060e61c5641581d8cbbd7b
Emilia <emilia.lach.91@gmail.com>
2020-11-30 21:23:02 +0000

    deploy to GH pages

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var path = require('path');
 
+
 module.exports = {
     entry: {
         app: './src/index.js',
