28ed51f2c6b98846f755d62403857302eac11206
Emilia <emilia.lach.91@gmail.com>
2020-12-01 23:02:29 +0000

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,146 +1,57 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const ASSET_PATH = process.env.ASSET_PATH || '/';
-
-
+var path = require('path');
 
 module.exports = {
     entry: {
-      
-      main: "./src/index.js",
-      
+        app: './src/index.js',
     },
-    
     output: {
-        filename: "[name].[contenthash].[bundle].js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: '',
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
     },
-
     module: {
-      
         rules: [
-         
             {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-              }
-            },
-  
-          {
-            test: /\.html$/i,
-            loader: 'html-loader',
-            options: { minimize: true }
-          },
-          
-          {
-            test: /\.css$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'sass-loader'
-            ],
-          },
-          {
-            test: /\.(png|jpe?g|gif|svg|ttf)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options:{
-            
-                  name:"[name].[contenthash].[ext]",
-                  outputPath: "imgs&fonts"
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
                 }
-              },
-            ],
-          },
-      
-        ],
-      },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: { minimize: true }
+                    }
+                ]
 
-      plugins : [
-
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+        ]
+    },
+    plugins: [
         new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
+            template: "./src/index.html",
+            filename: "./index.html"
         }),
-
-        new MiniCssExtractPlugin({
-          filename: '[name].[contenthash].css',
-          chunkFilename: '[id].[contenthash].css',
+        new MiniCssExtractPlugin ({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
         }),
-
-        new CleanWebpackPlugin({}),
-
-      ]
-}
-// const HtmlWebPackPlugin = require("html-webpack-plugin");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// var path = require('path');
-
-
-// module.exports = {
-//     entry: {
-//         app: './src/index.js',
-//     },
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'bundle.js',
-//         publicPath: '',
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: "babel-loader"
-//                 }
-//             },
-//             {
-//                 test: /\.html$/,
-//                 use: [
-//                     {
-//                         loader: "html-loader",
-//                         options: { minimize: true }
-//                     }
-//                 ]
-
-//             },
-//             {
-//                 test: /\.(png|jpe?g|gif|svg|ttf)$/i,
-//                 use: [
-//                   {
-//                     loader: 'url-loader',
-//                             'file-loader'
-//                         options: {
-//                             name: 'assets/images/[name].[ext]',
-//                             publicPath: '../'
-//                     }
-//                   },
-//                 ],
-//               },
-//             {
-//                 test: /\.css$/i,
-//                 use: [
-//                     "style-loader",
-//                     "css-loader"
-//                 ]
-//             },
-//         ]
-//     },
-//     plugins: [
-//         new HtmlWebPackPlugin({
-//             template: "./src/index.html",
-//             filename: "./index.html"
-//         }),
-//         new MiniCssExtractPlugin ({
-//             filename: "[name].css",
-//             chunkFilename: "[id].css"
-//         }),
-// ]}
+]}
 
